3d7a09deaf2930a3223352c4cb95945360f91a23
Zamprognog <g.zamprogno@vu.nl>
2023-11-19 13:22:29 +0100

    v1 works with pizza Margherita

--- reasoner.py
+++ reasoner.py
@@ -3,6 +3,7 @@ import sys
 import itertools
 import copy
 
+
 def load_onto(ontologyName):
     parser = gateway.getOWLParser()
     formatter = gateway.getSimpleDLFormatter()
@@ -19,9 +20,9 @@ def load_onto(ontologyName):
     return ontology
 
 
-def subsumers(ontologyName='pizza.owl', className='"American"'):
+def subsumers(ontologyName='pizza.owl', className='"Margherita"'):
     # IMPORTANT
-    # remember to have "" in the class name (NEED TO ADD FORMAT CHECK)
+    # remember to have "" in the class name (NEED TO append FORMAT CHECK)
 
     print(f'Finding the subsumers of class {className} in ontology {ontologyName}')
 
@@ -54,140 +55,154 @@ def subsumers(ontologyName='pizza.owl', className='"American"'):
     rolesByElement = dict()
     roles = dict()
 
-    conceptsByElement['d0'] = set()
-    rolesByElement['d0'] = set()
+    conceptsByElement['d0'] = []
+    rolesByElement['d0'] = []
     initialConcept = elFactory.getConceptName(className)
 
     # i don't like this but atm <concept> in <set> does not work
-    conceptsByElement['d0'].add(initialConcept)
+    conceptsByElement['d0'].append(initialConcept)
     dcounter = 1
 
     changed = True
 
     while changed:
         print('---')
-        changes = 0  # debug help
         changed = False
 
         # apply all the rules
-        possibleNewElements = dict()
+        possibleNewElements = dict()  # this if new elements are appended
         for d in conceptsByElement.keys():
             updatedConcepts = conceptsByElement[d].copy()
             # Top-rule
             if elFactory.getTop() not in conceptsByElement[d]:
-                print(f'top-rule: adding Top to {d}')
-                updatedConcepts.add(elFactory.getTop())
-                changes += 1
+                print(f'top-rule: appending Top to {d}')
+                updatedConcepts.append(elFactory.getTop())
                 changed = True
 
             # GCI-rule
             for gci in GCIaxioms:
                 if (gci.lhs() in conceptsByElement[d] and
                         gci.rhs() not in conceptsByElement[d]):
-                    print(f'GCI-rule: adding {formatter.format(gci.rhs())} to {d}')
-                    updatedConcepts.add(gci.rhs())
+                    print(f'GCI-rule: appending {formatter.format(gci.rhs())} to {d}')
+                    updatedConcepts.append(gci.rhs())
                     changed = True
 
+            # n-rule 1
             for c in conceptsByElement[d]:
 
-                # n-rule 1
                 if c.getClass().getSimpleName() == "ConceptConjunction":
                     if c.getConjuncts()[0] not in conceptsByElement[d]:
-                        print(f'CONJ-rule1: adding {formatter.format(c.getConjuncts()[0])} to {d}')
-                        updatedConcepts.add(c.getConjuncts()[0])
+                        print(f'CONJ-rule1: appending {formatter.format(c.getConjuncts()[0])} to {d}')
+                        updatedConcepts.append(c.getConjuncts()[0])
                         changed = True
                     if c.getConjuncts()[1] not in conceptsByElement[d]:
-                        print(f'CONJ-rule1: adding {formatter.format(c.getConjuncts()[1])} to {d}')
-                        updatedConcepts.add(c.getConjuncts()[1])
+                        print(f'CONJ-rule1: appending {formatter.format(c.getConjuncts()[1])} to {d}')
+                        updatedConcepts.append(c.getConjuncts()[1])
                         changed = True
-                # E-rule 1
-                elif c.getClass().getSimpleName() == "ExistentialRoleRestriction":
+
+            conceptsByElement[d] = updatedConcepts
+            updatedConcepts = conceptsByElement[d].copy()
+            # n-rule 2
+            for c1, c2 in itertools.permutations(conceptsByElement[d], 2):
+
+                conj = elFactory.getConjunction(c1, c2)
+                if conj in allConcepts and conj not in conceptsByElement[d]:
+                    print(f'CONJ-rule2: appending {formatter.format(conj)}')
+                    updatedConcepts.append(conj)
+                    changed = True
+
+            conceptsByElement[d] = updatedConcepts
+            updatedConcepts = conceptsByElement[d].copy()
+
+            #E-rule 1
+            #optimization-wise, not ideal, but each rule might be changing hte concepts
+            for c in conceptsByElement[d]:
+                if c.getClass().getSimpleName() == "ExistentialRoleRestriction":
 
                     role = c.role()
                     filler = c.filler()
 
                     introduced = False
 
+                    if d not in rolesByElement.keys():
+                        rolesByElement[d] = []
                     # look if already-present elements are suitable candidates
                     for r_succ_candidate in conceptsByElement.keys():
                         if filler in conceptsByElement[r_succ_candidate] and not introduced:
-                        # there is an element e with initial concept C --- need to verify if this works as intended
-                            if (role, filler) not in rolesByElement[d]:
-                                rolesByElement[d].add((role, r_succ_candidate))
+                            # there is an element e with initial concept C --- need to verify if this works as intended
+                            if (role, r_succ_candidate) not in rolesByElement[d]:
+                                rolesByElement[d].append((role, r_succ_candidate))
                                 changed = True
-                                print(f'E-rule1 introducing a {formatter.format(role)}-successor from {d} to {r_succ_candidate}')
+                                print(
+                                    f'E-rule1 introducing a {formatter.format(role)}-successor from {d} to {r_succ_candidate}')
                             introduced = True
 
                     if not introduced:
                         r_successor = f'd{dcounter}'
-                        # add a new r successor to d, assign the concept as initial concept
-                        possibleNewElements[r_successor] = {filler}
+                        # append a new r successor to d, assign the concept as initial concept
+                        possibleNewElements[r_successor] = [filler]
                         dcounter += 1
                         changed = True
-                        rolesByElement[d].add((role, r_successor))
-                        print(f'E-rule1 introducing a {formatter.format(role)}-successor from {d} to NEW element d{dcounter-1}')
-
-
-            # n-rule 2
-
-            for c1, c2 in itertools.permutations(conceptsByElement[d], 2):
-                conj = elFactory.getConjunction(c1, c2)
-                if conj in allConcepts and conj not in conceptsByElement[d]:
-                    print(f'CONJ-rule2: adding {formatter.format(conj)}')
-                    updatedConcepts.add(conj)
-                    changed = True
+                        rolesByElement[d].append((role, r_successor))
+                        print(
+                            f'E-rule1 introducing a {formatter.format(role)}-successor from {d} to NEW element d{dcounter - 1}')
 
             conceptsByElement[d] = updatedConcepts
+            updatedConcepts = conceptsByElement[d].copy()
 
-            # E-rule 2
-            for role, r_succ_candidate in rolesByElement[d]:
-                for r_succ_concept in conceptsByElement[r_succ_candidate]:
-                    newConcept = elFactory.getExistentialRoleRestriction(role, r_succ_concept)
-                    if newConcept not in conceptsByElement[d] and newConcept in allConcepts:
-                        updatedConcepts.add(newConcept)
-                        changed = True
-
-
+            #E-rule 2
+            if d in rolesByElement.keys():
+                for role, r_succ_candidate in rolesByElement[d]:
+                    if r_succ_candidate in conceptsByElement.keys():
+                        for r_succ_concept in conceptsByElement[r_succ_candidate]:
+                            newConcept = elFactory.getExistentialRoleRestriction(role, r_succ_concept)
+                            if newConcept not in conceptsByElement[d] and newConcept in allConcepts:
+                                updatedConcepts.append(newConcept)
+                                changed = True
+                        conceptsByElement[d] = updatedConcepts
+                    else:
+                        for r_succ_concept in possibleNewElements[r_succ_candidate]:
+                            newConcept = elFactory.getExistentialRoleRestriction(role, r_succ_concept)
+                            if newConcept not in conceptsByElement[d] and newConcept in allConcepts:
+                                updatedConcepts.append(newConcept)
+                                changed = True
+                        conceptsByElement[d] = updatedConcepts
 
         for elem in possibleNewElements.keys():
-            #probably updates need to happen before
+            # probably updates need to happen before
             conceptsByElement[elem] = possibleNewElements[elem]
-
-
-
 
     print('end')
 
 
 gateway = JavaGateway()
-# subsumers(sys.argv[1],sys.argv[2])
-subsumers('vuramen.ttl', 'NoukouChickenRamen')
-
-
+#subsumers(sys.argv[1],sys.argv[2])
+#subsumers('vuramen.ttl', 'NoukouChickenRamen')
+subsumers()
 # if role not in roles.keys():
-                    #     # this is a 'new' role
-                    #     roles[role] = []
-                    #
-                    # introduced = False
-                    #
-                    # # look if already-present elements are suitable candidates
-                    # for r_succ_candidate in conceptsByElement.keys():
-                    #     if filler in conceptsByElement[r_succ_candidate] and not introduced:
-                    #         # there is an element e with initial concept C --- need to verify if this works as intended
-                    #         if ((d, r_succ_candidate) not in roles[role]):
-                    #             roles[role].append((d, r_succ_candidate))
-                    #             changed = True
-                    #             print(f'E-rule1 introducing a {formatter.format(role)}-successor from {d} to {r_succ_candidate}')
-                    #         introduced = True
+#     # this is a 'new' role
+#     roles[role] = []
+#
+# introduced = False
+#
+# # look if already-present elements are suitable candidates
+# for r_succ_candidate in conceptsByElement.keys():
+#     if filler in conceptsByElement[r_succ_candidate] and not introduced:
+#         # there is an element e with initial concept C --- need to verify if this works as intended
+#         if ((d, r_succ_candidate) not in roles[role]):
+#             roles[role].append((d, r_succ_candidate))
+#             changed = True
+#             print(f'E-rule1 introducing a {formatter.format(role)}-successor from {d} to {r_succ_candidate}')
+#         introduced = True
 
-                    # if not introduced:
-                    #     r_successor = f'd{dcounter}'
-                    #     # add a new r successor to d, assign the concept as initial concept
-                    #     possibleNewElements[r_successor] = {filler}
-                    #     dcounter += 1
-                    #     changed = True
-                    #     roles[role].append((d, r_successor))
-                    #     print(f'E-rule1 introducing a {formatter.format(role)}-successor from {d} to NEW element d{dcounter-1}')
+# if not introduced:
+#     r_successor = f'd{dcounter}'
+#     # append a new r successor to d, assign the concept as initial concept
+#     possibleNewElements[r_successor] = {filler}
+#     dcounter += 1
+#     changed = True
+#     roles[role].append((d, r_successor))
+#     print(f'E-rule1 introducing a {formatter.format(role)}-successor from {d} to NEW element d{dcounter-1}')
 
 # for concept in allConcepts:
 #     print(concept)
